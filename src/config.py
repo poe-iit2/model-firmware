ed5e34173849db2c7f8f5ad9ffc29335ba07c0e1
wecreate1 <jimmy@sallenback.net>
2025-11-18 17:33:33 -0600

    Updated config for raspberry pi format

--- src/config.py
+++ src/config.py
@@ -1,14 +1,13 @@
-WIFI_SSID = "JimmyXPS"
-WIFI_PASSWORD = "ashvbiqbrglncawuhciuwli"
+import board
 
-GRAPHQL_HOST = "192.168.12.1"
+GRAPHQL_HOST = "localhost"
 GRAPHQL_PORT = 5000
 GRAPHQL_PATH = "/"
 
 led_pins = [
     {
-        "PIN": 25,
-        "COUNT": 102
+        "PIN": board.D18,
+        "COUNT": 120
     },
 ]
 
@@ -48,25 +47,10 @@ led_segments = [
 devices = (
     {
         "id": 0,
-        "DHT_PIN": 14,
-        # "PRESENCE_PIN": 35,
-        "PRESENCE_TX_PIN": 17,
-        "PRESENCE_RX_PIN": 16,
-        "PRESENCE_UART_CONTROLLER": 1,
-        # "LED_SEGMENTS": [],
         "LED_SEGMENTS": ["ROOM1R", "ROOM1L"],
-        "AIR_DIN_PIN": 34,
-        #"AIR_ADC_PIN": 32
+        # "AIR_DIN_PIN": ,
     }, {
         "id": 1,
-        "DHT_PIN": 27,
-        # "PRESENCE_PIN": 35,
-        "PRESENCE_TX_PIN": 18,
-        "PRESENCE_RX_PIN": 5,
-        "PRESENCE_UART_CONTROLLER": 2,
-        "AIR_DIN_PIN": 32,
-        # "AIR_ADC_PIN": 32,
-        # "LED_SEGMENTS": [],
         "LED_SEGMENTS": ["ROOM2R", "ROOM2L"],
-    }
+    },
 )
